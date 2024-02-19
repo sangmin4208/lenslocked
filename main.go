7fb0cc0424e0dd0aa37133f0c9961ef18fe28b46
sangmin <sangmin4208@gmail.com>
2024-02-19 13:20:12 +0900

    setup air

--- main.go
+++ main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
-
-type nacho struct{}
-
-func (na nacho) Write(p []byte) (n int, err error) {
-	fmt.Println(fmt.Sprintf("%s", p))
-	return len(p), nil
+	fmt.Fprint(w, "<h1>Welcome to my great site!!!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Server is running on port 3000")
-	fmt.Fprintln(os.Stdout, "Hello world")
-	fmt.Fprintln(nacho{}, "Hello world")
 	http.ListenAndServe(":3000", nil)
 }
