e2d41508ee35193998ba71c4e78ab47d70569073
sangmin <sangmin4208@gmail.com>
2024-03-12 23:46:23 +0900

    not found page

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		http.NotFound(w, r)
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 }
 
