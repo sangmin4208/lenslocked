62888982cf0a029a03903086c93e3e0c6d1baf07
sangmin <sangmin4208@gmail.com>
2024-03-12 23:14:02 +0900

    custom routing

--- main.go
+++ main.go
@@ -13,9 +13,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Server is running on port 3000!!")
 	http.ListenAndServe(":3000", nil)
 }
