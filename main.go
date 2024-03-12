e443aaea18a51ff6bec544cae4551312cc3f5c32
sangmin <sangmin4208@gmail.com>
2024-03-12 22:59:22 +0900

    creating a contact page

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my great site!!!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Server is running on port 3000")
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	fmt.Println("Server is running on port 3000!!")
 	http.ListenAndServe(":3000", nil)
 }
